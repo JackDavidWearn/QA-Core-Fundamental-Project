17c8733dc139a6fbde28ca0e54e7ca5fc9716629
JackDavidWearn <wearnjack@gmail.com>
2022-03-10 12:18:44 +0000

    changes to test.sh

--- test.sh
+++ test.sh
@@ -11,4 +11,4 @@ sudo apt-get install python3 python3-pip python3-venv -y
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov=application --cov-report html
+python3 -m pytest --cov --cov-report html
